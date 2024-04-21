e9c8fda8da5033235477a7de6ce69cbcc75dab26
Sufi-to <3thomas225@gmail.com>
2024-04-22 03:51:25 +0400

    am ur master

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# create a configuration file
+file_line { 'Turn off passwd':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
