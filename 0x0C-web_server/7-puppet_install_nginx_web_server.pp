67dfb8b11cfa8a6ede8a5e1194794dc8d0281be7
Sufi-to <3thomas225@gmail.com>
2024-04-23 21:30:19 +0400

    test the server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,15 @@
+# install and configure an Nginx server using puppet
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { 'var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'aaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
