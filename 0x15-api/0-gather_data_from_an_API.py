b3fb2cd97e201df72f71cda3ec558470ab6ebb88
Sufi-to <3thomas225@gmail.com>
2024-05-20 20:00:51 +0400

    get employes and tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script using a given REST API to get data back."""
+import requests as req
+from sys import argv
+import json
+
+
+def tasks_done(id):
+    """Calculate the number of completed tasks."""
+    url = "https://jsonplaceholder.typicode.com/todos"
+    res = req.get(url).text
+    obj = json.loads(res)
+    tasks_done = []
+    count = 0
+    for i in obj:
+        if i['userId'] == id and i['completed']:
+            count += 1
+            tasks_done.append(i['title'])
+    return count, tasks_done
+
+
+if __name__ == "__main__":
+    url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    res = req.get(url).text
+    obj = json.loads(res)
+    print(obj)
+    num_task, comp_tasks = tasks_done(obj['id'])
+    print(f"Employee {obj['name']} is done with tasks({num_task}/20):")
+    for i in comp_tasks:
+        print("/t {}".format(i))
