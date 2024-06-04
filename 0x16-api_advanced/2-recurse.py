80fbd859249bcf882085797102aa3c7f4d16ed3c
Sufi-to <3thomas225@gmail.com>
2024-06-04 23:25:51 +0400

    recursive call to the api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,35 +7,12 @@ subreddit, the function should return None.
 import requests
 
 
-# yo = requests.get(f"https://www.reddit.com/r/programming/hot.json")
-# x = yo.json()
-# posts = x.get("data", {}).get("children", [])
-# hot_list = [post["data"]["title"] for post in posts]
-# print(len(hot_list))
-# def recurse(subreddit, after=None, hot_list=[]):
-#     yo = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json",
-#                         allow_redirects=False)
-#     if yo.status_code == 200:
-#         x = yo.json()
-#         posts = x.get("data", {}).get("children", [])
-#         hot_list.extend([post["data"]["title"] for post in posts])
-#         after = x.get("data", {}).get("after")
-#         if after:
-#             return recurse(subreddit, after, hot_list)
-#         else:
-#             return hot_list
-#     else:
-#         return (None)
 def recurse(subreddit, after=None, hot_list=None):
     if hot_list is None:
         hot_list = []
-
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {"after": after} if after else {}
-
-
     response = requests.get(url, params=params, allow_redirects=False)
-
     if response.status_code == 200:
         x = response.json()
         posts = x.get("data", {}).get("children", [])
