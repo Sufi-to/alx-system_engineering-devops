d2857690c35a6da54602a111cf888e6f633595c9
Sufi-to <3thomas225@gmail.com>
2024-06-04 20:18:07 +0400

    get subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-Script the queries the Reddit api and return the number of subscribers
-(not active users, total subscribers) for a given subreddit
+Script the queries the Reddit api and return the number of subscribers.
+(not active users, total subscribers) for a given subreddit.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    yo = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
-    if yo.status_code == 200:
-        x = yo.json()
+    """Get the number of subscribers."""
+    reddit = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
+    if reddit.status_code == 200:
+        x = reddit.json()
         return (x['data']['subscribers'])
     return (0)
