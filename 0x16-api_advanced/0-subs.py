e524e353e5d5876ed6153769a452603fb73efe0e
Sufi-to <3thomas225@gmail.com>
2024-06-06 22:18:16 +0400

    hell yeah

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,16 +10,14 @@ def number_of_subscribers(subreddit):
     """Get the number of subscribers."""
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    user_agent = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    user_agent = {"User-agent": "Mozilla/5.0"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     try:
         reddit = requests.get(url, headers=user_agent, allow_redirects=False)
         if reddit.status_code == 200:
             x = reddit.json()
-            print("OK")
-            return (x.get("data", {}).get("subscribers", 0))
+            return x.get("data", {}).get("subscribers", 0)
         else:
-            print("OK")
-            return (0)
+            return 0
     except requests.RequestException as e:
-        return (0)
+        return 0
