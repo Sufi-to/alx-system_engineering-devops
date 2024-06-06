0a1029340aa6b48e3ac517a4434ff230b9c9a81f
Sufi-to <3thomas225@gmail.com>
2024-06-06 19:36:47 +0400

    sub count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,16 +8,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Get the number of subscribers."""
-    if subreddit is None or type(subreddit) is not str:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
     user_agent = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    reddit = requests.get(url, headers=user_agent, allow_redirects=False)
-    if reddit.status_code == 200:
-        try:
+    try:
+        reddit = requests.get(url, headers=user_agent, allow_redirects=False)
+        if reddit.status_code == 200:
             x = reddit.json()
-            return (x.get("data", {}).get("subscribers"))
-        except ValueError:
+            return (x.get("data", {}).get("subscribers", 0))
+        else:
             return (0)
-    else:
+    except requests.RequestException as e:
+        return (0)
+    except ValueError:
         return (0)
