612b0413b49df7ade92b40a53ead802eced89ded
Sufi-to <3thomas225@gmail.com>
2024-06-06 04:40:53 +0400

    subs man

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Get the number of subscribers."""
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
+
     user_agent = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     reddit = requests.get(url, headers=user_agent, allow_redirects=False)
