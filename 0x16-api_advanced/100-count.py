8845a2cf54ca4c9f752d17482deae030284b8385
Sufi-to <3thomas225@gmail.com>
2024-06-06 23:15:07 +0400

    count words

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Script that counts the number of give words occurrences in the hot titles"""
+import requests
+from requests import get
+
+
+def count_words(subreddit, word_list, after="", w_dic={}):
+    """Returns the word counts."""
+    red = "https://www.reddit.com/r/"
+    headers = {'user-agent': 'my-agent'}
+    if not w_dic:
+        for word in word_list:
+            w_dic[word] = 0
+
+    if after is None:
+        word_list = [[key, value] for key, value in w_dic.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for w in word_list:
+            if w[1]:
+                print("{}: {}".format(w[0].lower(), w[1]))
+        return None
+    url = red + "{}/hot/.json".format(subreddit)
+    params = {'limit': 100, 'after': after}
+    fr = get(url, headers=headers, params=params, allow_redirects=False)
+    if fr.status_code != 200:
+        return None
+    try:
+        js = fr.json()
+    except ValueError:
+        return None
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            title = post.get("title")
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                w_dic[w] += lower.count(w.lower())
+
+    except requests.RequestException as e:
+        return None
+
+    count_words(subreddit, word_list, after, w_dic)
