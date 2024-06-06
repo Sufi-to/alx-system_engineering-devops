1b3944c2dee00c57c9ab67b8e40c120cef125803
Sufi-to <3thomas225@gmail.com>
2024-06-06 04:36:55 +0400

    subs number

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Get the number of subscribers."""
+    user_agent = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    reddit = requests.get(url, allow_redirects=False)
+    reddit = requests.get(url, headers=user_agent, allow_redirects=False)
     if reddit.status_code == 200:
         x = reddit.json()
         return (x.get("data", {}).get("subsribers"))
